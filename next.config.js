4f2f57155a2172619893ddc1c2eb1714e64f6fcb
danielchochlinski <89450753+danielchochlinski@users.noreply.github.com>
2022-02-01 09:52:01 +0000

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  env: {
-    FETCH_URL:
-      "https://restau-8b045-default-rtdb.europe-west1.firebasedatabase.app/meals.json",
-    SEND_ORDER_URL:
-      "https://restau-8b045-default-rtdb.europe-west1.firebasedatabase.app/orders.json",
-  },
-  reactStrictMode: true,
-};
